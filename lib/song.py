7e51d1edee128be5969bf049cb9a5ced5ea8043a
Jamie Bryson <jamieab98@gmail.com>
2026-01-11 15:16:33 -0500

    removed the return statements and the code used to help me test the functionality

--- lib/song.py
+++ lib/song.py
@@ -16,19 +16,16 @@ class Song:
     @classmethod
     def add_song_to_count(cls):
         cls.song_count += 1
-        return cls.song_count
     
     @classmethod
     def add_to_genres(cls, genre):
         cls.add_to_genre_count(genre)
         cls.existing_genres.add(genre)
-        return cls.existing_genres
     
     @classmethod
     def add_to_artists(cls, artist):
         cls.add_to_artists_count(artist)
         cls.existing_artists.add(artist)
-        return(cls.existing_artists)
     
     @classmethod
     def add_to_genre_count(cls, genre):
@@ -48,5 +45,3 @@ Song1 = Song("Sunday Candy", "Chance the Rapper", "Rap")
 Song2 = Song("Man I need", "Olivia Dean", "Pop")
 Song3 = Song("Sunday In Brooklyn", "Kota the Friend", "Rap")
 Song4 = Song("Colorado", "Kota the Friend", "Lofi")
-
-print(Song.artist_count)
