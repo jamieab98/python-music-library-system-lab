5aaa0376c890001a8b3e3beb78a7e8a5844afa7f
Jamie Bryson <jamieab98@gmail.com>
2026-01-11 15:42:45 -0500

    Changed some naming of attributes

--- lib/song.py
+++ lib/song.py
@@ -1,7 +1,7 @@
 class Song:
-    song_count = 0
-    existing_artists = set()
-    existing_genres = set()
+    count = 0
+    artists = set()
+    genres = set()
     genre_count = {}
     artist_count = {}
 
@@ -15,33 +15,35 @@ class Song:
     
     @classmethod
     def add_song_to_count(cls):
-        cls.song_count += 1
+        cls.count += 1
     
     @classmethod
     def add_to_genres(cls, genre):
+        cls.genres.add(genre)
         cls.add_to_genre_count(genre)
-        cls.existing_genres.add(genre)
     
     @classmethod
     def add_to_artists(cls, artist):
+        cls.artists.add(artist)
         cls.add_to_artists_count(artist)
-        cls.existing_artists.add(artist)
     
     @classmethod
     def add_to_genre_count(cls, genre):
-        if genre in cls.existing_genres:
-            cls.genre_count[genre] += 1
-        else:
+        if genre not in cls.genre_count:
             cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
     
     @classmethod
     def add_to_artists_count(cls, artist):
-        if artist in cls.existing_artists:
-            cls.artist_count[artist] += 1
-        else:
+        if artist not in cls.artist_count:
             cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
 
 Song1 = Song("Sunday Candy", "Chance the Rapper", "Rap")
 Song2 = Song("Man I need", "Olivia Dean", "Pop")
 Song3 = Song("Sunday In Brooklyn", "Kota the Friend", "Rap")
 Song4 = Song("Colorado", "Kota the Friend", "Lofi")
+
+print(Song.count)
