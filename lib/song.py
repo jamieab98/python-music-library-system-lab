896797fc0db4c77cea92e22b85be3854c4fd5e05
Jamie Bryson <jamieab98@gmail.com>
2026-01-11 14:17:01 -0500

    Created the feature branch and the basics of the Song Class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,12 @@
 class Song:
+    song_count = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        print(self.name, self.artist, self.genre)
+    
     pass
+
+Song1 = Song("Sunday Candy", "Chance the Rapper", "Soulful Rap")
