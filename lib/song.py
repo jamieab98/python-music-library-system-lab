bb75cf0ab9eb9a05852051bb94128bc93a346c78
Jamie Bryson <jamieab98@gmail.com>
2026-01-11 15:14:27 -0500

    created the add_to_genre/artist_count methods

--- lib/song.py
+++ lib/song.py
@@ -2,6 +2,8 @@ class Song:
     song_count = 0
     existing_artists = set()
     existing_genres = set()
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -18,17 +20,33 @@ class Song:
     
     @classmethod
     def add_to_genres(cls, genre):
+        cls.add_to_genre_count(genre)
         cls.existing_genres.add(genre)
         return cls.existing_genres
+    
     @classmethod
     def add_to_artists(cls, artist):
+        cls.add_to_artists_count(artist)
         cls.existing_artists.add(artist)
         return(cls.existing_artists)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.existing_genres:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+    
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        if artist in cls.existing_artists:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
 
 Song1 = Song("Sunday Candy", "Chance the Rapper", "Rap")
 Song2 = Song("Man I need", "Olivia Dean", "Pop")
 Song3 = Song("Sunday In Brooklyn", "Kota the Friend", "Rap")
 Song4 = Song("Colorado", "Kota the Friend", "Lofi")
-print(Song.song_count)
-print(Song.existing_genres)
-print(Song.existing_artists)
+
+print(Song.artist_count)
