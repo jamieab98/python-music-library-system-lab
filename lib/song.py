2f1e81efac8e7a8f79ac87d22ce4fc054f660742
Jamie Bryson <jamieab98@gmail.com>
2026-01-11 14:35:12 -0500

    created the add_song_to_count class method

--- lib/song.py
+++ lib/song.py
@@ -1,12 +1,24 @@
 class Song:
     song_count = 0
+    existing_artists = set()
+    existing_genres = set()
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-        print(self.name, self.artist, self.genre)
+        self.add_song_to_count()
+    
+    @classmethod
+    def add_song_to_count(cls):
+        cls.song_count += 1
+        return cls.song_count
+    
+    @classmethod
+    def add_to_genres(cls):
+        
     
     pass
 
-Song1 = Song("Sunday Candy", "Chance the Rapper", "Soulful Rap")
+Song1 = Song("Sunday Candy", "Chance the Rapper", "Rap")
+print(Song.song_count)
